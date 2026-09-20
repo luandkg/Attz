d6ffce9a789fa20a76aed5e217067ff669102fdf
Luan Freitas <luandkg@gmail.com>
2026-09-20 09:40:54 -0300

    test(QTTCacheVer): melhora execução do teste

--- src/testes/TesteComQTTCacheVer.java
+++ src/testes/TesteComQTTCacheVer.java
@@ -19,29 +19,31 @@ import libs.tronarko.Tronarko;
 public class TesteComQTTCacheVer {
 
 
-    public static void init(){
+    public static void init() {
 
         AtzumTerra planeta = new AtzumTerra();
         Cores mCores = new Cores();
 
 
         Tron t0_a = Tronarko.getTronAgora();
-        QTTCacheVer dados_omega = new QTTCacheVer(AtzumCreator.DADOS_GET_ARQUIVO("regioes.qtt"),500);
+        QTTCacheVer dados_omega = new QTTCacheVer(AtzumCreator.DADOS_GET_ARQUIVO("regioes.qtt"), 500);
         Renderizador render_omega = Renderizador.construir(planeta.getLargura(), planeta.getAltura(), mCores.getPreto());
 
-        for(Retangulo zona : dados_omega.getZonas()){
-            //fmt.print("{} - {} :: {} - {}",zona.getX(),zona.getY(),zona.getX2(),zona.getY2());
+        fmt.print("ZONAS = {}", dados_omega.getZonas().getQuantidade());
+
+        for (Retangulo zona : dados_omega.getZonas()) {
+            fmt.print("\t ++ ZONA :: {} - {} :: {} - {}", zona.getX(), zona.getY(), zona.getX2(), zona.getY2());
 
 
-            Lista<Par<Ponto,Integer>> zona_dados = dados_omega.getZonaDados(zona);
+            Lista<Par<Ponto, Integer>> zona_dados = dados_omega.getZonaDados(zona);
 
-            for(Par<Ponto,Integer> pt : zona_dados){
+            for (Par<Ponto, Integer> pt : zona_dados) {
                 if (planeta.isTerra(pt.getChave().getX(), pt.getChave().getY())) {
 
-                    int valor =pt.getValor();
-                    if(valor>0){
-                        valor=valor*15;
-                        render_omega.setPixel(pt.getChave().getX(), pt.getChave().getY(),new Cor(valor,valor,valor));
+                    int valor = pt.getValor();
+                    if (valor > 0) {
+                        valor = valor * 15;
+                        render_omega.setPixel(pt.getChave().getX(), pt.getChave().getY(), new Cor(valor, valor, valor));
                     }
 
 
@@ -51,16 +53,15 @@ public class TesteComQTTCacheVer {
         }
 
 
-
         Imagem.exportar(render_omega.toImagemSemAlfa(), AtzumCreator.LOCAL_GET_ARQUIVO("teste_omega.png"));
         Tron t0_b = Tronarko.getTronAgora();
 
-        TEMPO("T0",t0_a,t0_b);
+        TEMPO("T0", t0_a, t0_b);
 
 
         Tron t1_a = Tronarko.getTronAgora();
-        QTT dados_alfa =  QTT.getTudo((AtzumCreator.DADOS_GET_ARQUIVO("regioes.qtt")));
-        fmt.print("Dados : {} - {}",dados_alfa.getLargura(),dados_alfa.getAltura());
+        QTT dados_alfa = QTT.getTudo((AtzumCreator.DADOS_GET_ARQUIVO("regioes.qtt")));
+        fmt.print("Dados : {} - {}", dados_alfa.getLargura(), dados_alfa.getAltura());
 
 
         Renderizador render_alfa = Renderizador.construir(planeta.getLargura(), planeta.getAltura(), mCores.getPreto());
@@ -69,44 +70,48 @@ public class TesteComQTTCacheVer {
             for (int x = 0; x < planeta.getLargura(); x++) {
                 if (planeta.isTerra(x, y)) {
 
-                    int valor = dados_alfa.getValor(x,y);
-                    if(valor>0){
-                        valor=valor*15;
-                        render_alfa.setPixel(x,y,new Cor(valor,valor,valor));
+                    int valor = dados_alfa.getValor(x, y);
+                    if (valor > 0) {
+                        valor = valor * 15;
+                        render_alfa.setPixel(x, y, new Cor(valor, valor, valor));
                     }
 
-                }}}
+                }
+            }
+        }
 
 
         Imagem.exportar(render_alfa.toImagemSemAlfa(), AtzumCreator.LOCAL_GET_ARQUIVO("teste_alfa.png"));
         Tron t1_b = Tronarko.getTronAgora();
 
-        TEMPO("T1",t1_a,t1_b);
+        TEMPO("T1", t1_a, t1_b);
 
 
         Tron t2_a = Tronarko.getTronAgora();
 
-        QTTCacheVer dados_beta = new QTTCacheVer(AtzumCreator.DADOS_GET_ARQUIVO("regioes.qtt"),500);
+        QTTCacheVer dados_beta = new QTTCacheVer(AtzumCreator.DADOS_GET_ARQUIVO("regioes.qtt"), 500);
         Renderizador render_beta = Renderizador.construir(planeta.getLargura(), planeta.getAltura(), mCores.getPreto());
 
         for (int y = 0; y < planeta.getAltura(); y++) {
             for (int x = 0; x < planeta.getLargura(); x++) {
                 if (planeta.isTerra(x, y)) {
 
-                    int valor = dados_beta.get(x,y);
-                    if(valor>0) {
-                        valor=valor*15;
-                        render_beta.setPixel(x,y,new Cor(valor,valor,valor));
+                    int valor = dados_beta.get(x, y);
+                    if (valor > 0) {
+                        valor = valor * 15;
+                        render_beta.setPixel(x, y, new Cor(valor, valor, valor));
                     }
 
 
-                }}}
+                }
+            }
+        }
 
 
         Imagem.exportar(render_beta.toImagemSemAlfa(), AtzumCreator.LOCAL_GET_ARQUIVO("teste_beta.png"));
         Tron t2_b = Tronarko.getTronAgora();
 
-        TEMPO("T2",t2_a,t2_b);
+        TEMPO("T2", t2_a, t2_b);
 
         Tron t3_a = Tronarko.getTronAgora();
         Renderizador render_gama = Renderizador.construir(planeta.getLargura(), planeta.getAltura(), mCores.getPreto());
@@ -114,36 +119,28 @@ public class TesteComQTTCacheVer {
             for (int x = 0; x < planeta.getLargura(); x++) {
                 if (planeta.isTerra(x, y)) {
 
-                    int valor = QTT.pegar(AtzumCreator.DADOS_GET_ARQUIVO("regioes.qtt"),x,y);
+                    int valor = QTT.pegar(AtzumCreator.DADOS_GET_ARQUIVO("regioes.qtt"), x, y);
 
-                    if(valor>0) {
-                        valor=valor*15;
-                        render_gama.setPixel(x,y,new Cor(valor,valor,valor));
+                    if (valor > 0) {
+                        valor = valor * 15;
+                        render_gama.setPixel(x, y, new Cor(valor, valor, valor));
                     }
 
 
-                }}}
+                }
+            }
+        }
 
 
         Imagem.exportar(render_gama.toImagemSemAlfa(), AtzumCreator.LOCAL_GET_ARQUIVO("teste_gama.png"));
         Tron t3_b = Tronarko.getTronAgora();
 
-        TEMPO("T3",t3_a,t3_b);
+        TEMPO("T3", t3_a, t3_b);
     }
 
 
-    public static void TEMPO(String nome,Tron t1,Tron t2){
-
-        Tron iniciado =t1;
-        Tron terminado = t2;
-
-        if(iniciado.getTozte().isIgual(terminado.getTozte())){
-
-            long uzzons = Tronarko.HAZDE_DIFERENCA(terminado.getHazde(),iniciado.getHazde());
-            fmt.print("{} >>> + {} uz",nome,uzzons);
-
-        }
-
+    public static void TEMPO(String nome, Tron t1, Tron t2) {
+        fmt.print("{} >>> + {}", nome, Tronarko.TRON_DIFERENCA(t1, t2));
     }
 
 }
