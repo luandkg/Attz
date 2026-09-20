8e1538b8315f6760127cbb14f69af21b0f1cb28a
Luan Freitas <luandkg@gmail.com>
2026-09-20 09:46:55 -0300

    style(MundoReal): melhora legibilidade

--- src/servicos/MundoReal.java
+++ src/servicos/MundoReal.java
@@ -12,10 +12,10 @@ public class MundoReal {
 
         String nome_completo = "";
 
-        Vetor<String> primeiro_nome = Vetor.CRIAR("Luan", "Alice", "Bruno", "Flavio", "Pedro", "Matheus", "Thiado", "Samara", "Alana", "Aglicia", "Marta", "Maria", "Madalena","João","Alberto","Gustado","Rodolfo","Sara","Carla","Jéssica");
+        Vetor<String> primeiro_nome = Vetor.CRIAR("Luan", "Alice", "Bruno", "Flavio", "Pedro", "Matheus", "Thiado", "Samara", "Alana", "Aglicia", "Marta", "Maria", "Madalena", "João", "Alberto", "Gustado", "Rodolfo", "Sara", "Carla", "Jéssica");
         Lista<String> sobrenome_lista_alfa = Lista.CRIAR("Bucar", "Alencar", "Freitas", "Diniz", "Coimbra", "Ferraz", "Almeida", "Oliveira", "Santos", "Silva", "Souza", "Pereira", "Ferreira");
 
-        nome_completo = Aleatorio.escolha_um(primeiro_nome)+" "+Aleatorio.escolha_um_sem_repetir(sobrenome_lista_alfa)+" "+Aleatorio.escolha_um_sem_repetir(sobrenome_lista_alfa);
+        nome_completo = Aleatorio.escolha_um(primeiro_nome) + " " + Aleatorio.escolha_um_sem_repetir(sobrenome_lista_alfa) + " " + Aleatorio.escolha_um_sem_repetir(sobrenome_lista_alfa);
 
         return Strings.CAPTALIZAR_FRASE(nome_completo);
     }
@@ -25,9 +25,9 @@ public class MundoReal {
 
         for (int i = 0; i < tam; i++) {
 
-            if(i==3 || i==6||i==9) {
+            if (i == 3 || i == 6 || i == 9) {
                 documento += ".";
-            }else             if(i==12){
+            } else if (i == 12) {
                 documento += "-";
             }
 
